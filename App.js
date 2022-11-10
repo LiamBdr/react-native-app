2d1f5c57cd49640206d10fb9d890f85329ff1a31
Liam Boudraa <liam.boudraa@etudiant.univ-reims.fr>
2022-11-10 10:32:41 +0100

    Feat - Homepage with navbar

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+import HomeScreen from './src/pages/HomeScreen';
+
+function SettingsScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} options={{
+                    title: 'My profile',
+                    tabBarIcon: ({size,focused,color}) => {
+                        return (
+                            <Image
+                                style={{ width: size, height: size }}
+                                source={{
+                                    uri:'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Burger_King_logo_%281999%29.svg/2024px-Burger_King_logo_%281999%29.svg.png',
+                                }}
+                            />
+                        );
+                    },
+                }} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        {/*(...) */}
+        <MyTabs />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
---
+++ src/pages/HomeScreen.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function HomeScreen() {
+    return (
+      <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </SafeAreaView>
+    );
+  }
+
+  export default HomeScreen;
