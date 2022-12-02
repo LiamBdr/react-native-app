f29545fe0a1d2f66f5f08a22ef801a7b91dae65e
Liam Boudraa <liam.boudraa@etudiant.univ-reims.fr>
2022-12-02 09:03:21 +0100

    Feat - Bottom nav

--- src/routes/TabComponent.js
+++ src/routes/TabComponent.js
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import { Image } from 'react-native';
+import { Image, Text } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CharacterList from '../screens/CharacterScreens/CharacterList';
 import FavoriteList from '../screens/FavoriteScreens/FavoriteList';
+import { FontAwesome5 } from '@expo/vector-icons'; 
+import { SimpleLineIcons } from '@expo/vector-icons';
 
 const TabComponent = () => {
 
@@ -13,35 +15,30 @@ const TabComponent = () => {
             initialRouteName="Liste"
             screenOptions={{
                 headerMode: 'screen',
-                headerTitle : 'Breaking Bad',
+                headerTitle: () => (
+                    <Image style={{ width: 80, height: '90%', resizeMode: 'contain', alignSelf: 'center' }}
+                    source={require('../../assets/images/breaking-bad-logo.png')}
+                    />
+                ),
+                headerStyle: {
+                    backgroundColor: 'white',
+                    borderBottomColor: 'rgb(50,95,55)',
+                    borderBottomWidth: 2,
+                    height: 95,
+                },  
             }}
-            
         >
-            <Tab.Screen name="Liste" component={CharacterList} options={{
-                tabBarIcon: ({ size, focused, color }) => {
-                    return (
-                        <Image
-                            style={{ width: size, height: size }}
-                            source={{
-                                uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Burger_King_logo_%281999%29.svg/2024px-Burger_King_logo_%281999%29.svg.png',
-                            }}
-                        />
-                    );
-                },
+            <Tab.Screen name="Liste" component={CharacterList} 
+                options={{
+                tabBarIcon: ({ size, focused, color }) => <FontAwesome5 name="caravan" size={24} color={focused ? "#fcd303" : "#325f37"} />,
+                tabBarLabel: ({focused}) => <Text style={{ fontSize: 12, fontWeight: 'bold', color: focused ? "black" : "#325f37" }}> Home </Text> ,
             }} />
             <Tab.Screen name="Favoris" component={FavoriteList}
                 options={{
-                    tabBarIcon: ({ size, focused, color }) => {
-                        return (
-                            <Image
-                                style={{ width: size, height: size }}
-                                source={{
-                                    uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzL4MUBJZmrRYt_3TO3Ta3dRNudHunq4N9L4NsDBsH&s',
-                                }}
-                            />
-                        );
-                    },
-                }} />
+                    tabBarIcon: ({ size, focused, color }) => <SimpleLineIcons name="chemistry" size={24} color={focused ? "#fcd303" : "#325f37"} />,
+                    tabBarLabel: ({focused}) => <Text style={{ fontSize: 12, fontWeight: 'bold', color: focused ? "black" : "#325f37" }}> Liste </Text> ,
+                }}  />
+            
         </Tab.Navigator>
     );
 }
