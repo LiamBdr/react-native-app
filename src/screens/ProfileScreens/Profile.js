d87244b76db94b9bcb2b9ba01499554104aa40c2
Liam Boudraa <liam.boudraa@etudiant.univ-reims.fr>
2023-02-19 15:45:53 +0100

    Feat - profil image upload

--- src/screens/ProfileScreens/Profile.js
+++ src/screens/ProfileScreens/Profile.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { SimpleLineIcons } from '@expo/vector-icons';
-import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TextInput, TouchableOpacity, Platform } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as ImagePicker from 'expo-image-picker';
+
 
 const Profile = () => {
+    const [profileImage, setProfileImage] = useState(null);
     const [favoris, setFavoris] = useState();
     const [favorisLength, setFavorisLength] = useState(0);
     const [name, setName] = useState('');
     const [age, setAge] = useState('');
-    const [location, setLocation] = useState('');
+    const [location, setLocation] = useState(''); ``
 
     useEffect(() => {
         AsyncStorage.getItem('favoris').then((value) => {
@@ -18,42 +21,85 @@ const Profile = () => {
             }
         });
         getUserInfo();
+        getImage();
     });
 
     const getUserInfo = async () => {
         try {
-          const value = await AsyncStorage.getItem('user');
-          if (value !== null) {
-            const user = JSON.parse(value);
-            setName(user.name);
-            setAge(user.age);
-            setLocation(user.location);
-          }
+            const value = await AsyncStorage.getItem('user');
+            if (value !== null) {
+                const user = JSON.parse(value);
+                setName(user.name);
+                setAge(user.age);
+                setLocation(user.location);
+            }
         } catch (e) {
-          console.warn(e);
+            console.warn(e);
         }
-      };
-    
-      const saveChanges = async () => {
+    };
+
+    const saveChanges = async () => {
         try {
-          await AsyncStorage.setItem('user', JSON.stringify({
-            name,
-            age,
-            location,
-          }));
+            await AsyncStorage.setItem('user', JSON.stringify({
+                name,
+                age,
+                location,
+            }));
         } catch (e) {
-          console.warn(e);
+            console.warn(e);
         }
-      };
+    };
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setProfileImage(result.assets[0].uri);
+            saveImage();
+        }
+    };
+
+    const saveImage = async () => {
+        try {
+            await AsyncStorage.setItem('image', JSON.stringify({
+                profileImage,
+            }));
+        }
+        catch (e) {
+            console.warn(e);
+        }
+    };
+
+    const getImage = async () => {
+        if (profileImage === null) {
+            try {
+                const value = await AsyncStorage.getItem('image');
+                if (value !== null) {
+                    setProfileImage(JSON.parse(value).profileImage);
+                }
+            } catch (e) {
+                console.warn(e);
+            }
+        }
+    };
 
     return (
         <View style={styles.container}>
-            <Image
-                style={styles.profileImage}
-                source={{
-                    uri: 'https://i.pinimg.com/originals/99/3a/ff/993aff69b6c079200f0f1120ced97113.jpg',
-                }}
-            />
+            <View style={styles.profile}>
+                <Image
+                    style={styles.profileImage}
+                    source={profileImage === null ? { uri: 'https://i.pinimg.com/originals/99/3a/ff/993aff69b6c079200f0f1120ced97113.jpg' } : { uri: profileImage }}
+
+                />
+                <TouchableOpacity onPress={pickImage} style={styles.uploadButton}>
+                    <Image source={require('../../../assets/images/upload-photo.png')} style={{ width: 20, height: 20 }} />
+                </TouchableOpacity>
+            </View>
             <View style={styles.likesContainer}>
                 <Text style={styles.likesText}>
                     {favorisLength} favoris
@@ -94,6 +140,25 @@ const styles = {
         flex: 1,
         alignItems: 'center',
     },
+    profile: {
+        width: '100%',
+        height: 200,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'relative',
+    },
+    uploadButton: {
+        position: 'absolute',
+        width: 35,
+        height: 35,
+        right: 70,
+        backgroundColor: 'lightgrey',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 20,
+    },
     profileImage: {
         width: 130,
         height: 130,
@@ -105,6 +170,7 @@ const styles = {
         flexDirection: 'row',
         alignItems: 'center',
         marginBottom: 20,
+        marginTop: 20,
     },
     likesText: {
         fontSize: 16,
